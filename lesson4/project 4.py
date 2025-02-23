6ea29ac2a1042e90c230404daba256cb20377cb0
1nawaalcoding <126.nawaal-mir@foundationworldschool.com>
2025-02-23 16:22:18 +0530

    project 4.py

---
+++ lesson4/project 4.py
@@ -0,0 +1,4 @@
+#calculating squareroot with python
+num_1=int(input("enter the number you want to find the square root of "))
+square_root=int(num_1**0.5)
+print("The square root of the number is ",square_root)
