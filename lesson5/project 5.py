6c1baff8b20be4dc8a893ada4e3311581f0f1737
1nawaalcoding <126.nawaal-mir@foundationworldschool.com>
2025-02-24 09:42:49 +0530

    project 5.py

---
+++ lesson5/project 5.py
@@ -0,0 +1,8 @@
+temp=float(input("enter the current temprature "))
+if temp>=25:
+    print("It is warm outside, you can wear light clothes")
+else: 
+    if temp>=15:
+     print("It is moderate outside, a light jacket might be needed")
+    else:
+     print("It is cold outside, wear warm clothes")
