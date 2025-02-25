2900d1ee3a823f5b78f6ebb375fea595ca64d312
1nawaalcoding <126.nawaal-mir@foundationworldschool.com>
2025-02-25 18:50:08 +0530

    lesson6

---
+++ lesson6/and or operator.py
@@ -0,0 +1,23 @@
+a=6
+b=3
+c=0
+if a and b and c:
+    print("all numbers have boolean value as true")
+else:
+    print("atleast one of the numbers has boolean value as false")
+
+g=6
+f=-8
+h=0
+if g>0 or f>0:
+   
+    print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
+
+if f>0 or h>0:
+        print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
+
+
---
+++ lesson6/not equals to.py
@@ -0,0 +1,19 @@
+a=11
+b=16
+c=16
+print(a!=b)
+print(b!=c)
+a="python"
+b="coding"
+if a!=b:
+    print("a and b are different")
+a=6
+b=9
+if (a==1)!=(b==9):
+    print("hi")
+a=int(input("enter a number"))
+if a%2!=0:
+    print("it is not a even number")
+    
+
+
