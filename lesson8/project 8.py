b8aa82cc41dc5b6ba795b73a2a590abefbcfb352
1nawaalcoding <126.nawaal-mir@foundationworldschool.com>
2025-03-03 16:16:46 +0530

    project 8.py

---
+++ lesson8/project 8.py
@@ -0,0 +1,6 @@
+a=int(input("enter a number(a):"))
+b=int(input("enter a number(b):"))
+c=int(input("enter a number(c):"))
+print("The actual numbers are", a,b,c)
+swapped_numbers=b,c,a
+print('the numbers after swapping are',swapped_numbers)
