e373eb9bdeea98ee04730d48dbe8ac0983cab594
1nawaalcoding <126.nawaal-mir@foundationworldschool.com>
2025-03-03 16:57:38 +0530

    project 6.py

--- lesson6/project 6.py
+++ lesson6/project 6.py
@@ -1,2 +1,8 @@
-character=str(input("enter your character"))
-if character==s
+character=str(input("enter your character:"))
+if len(character)==1:
+  if ("A"<=character<="Z") or ("a"<=character<="z"):
+    print(character,"is a alphabet")
+  else:
+    print(character,"is not a alphabet")
+else:
+  print("please enter only one character")
