d069b667b22f092f1da3fc83ef92784cb2ddb088
1nawaalcoding <126.nawaal-mir@foundationworldschool.com>
2025-03-04 18:41:45 +0530

    lesson10

---
+++ lesson10/reverse string.py
@@ -0,0 +1,6 @@
+string=input("please enter the string you want to reverse:")
+string2=''
+for i in string:
+    string2=i+string2
+print("the original string is",string)
+print("the reversed string is",string2)
---
+++ lesson9/project 9.py
@@ -0,0 +1,6 @@
+age=int(input("enter your age:"))
+range_of_age= 10 or 11 or 12 or 13 or 14 or 15 or 16 or 17 or 18 or 19 or 20
+if age==range_of_age:
+   print("your age is from 10 to 20 years old")
+else:
+   print("your age is not from 10 to 20 yrs old")
