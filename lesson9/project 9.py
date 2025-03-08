2cb0cfb4e80f5bfcfb6645346368d2e60e54595e
1nawaalcoding <126.nawaal-mir@foundationworldschool.com>
2025-03-08 17:04:51 +0530

    project 9.py

--- lesson9/project 9.py
+++ lesson9/project 9.py
@@ -1,5 +1,5 @@
 age=int(input("enter your age:"))
-if age>10 & age<20:
+if age>10 and age<20:
     print("your age is from 10 to 20 years old")
 else:
     print("your age is not from 10 to 20 years old")
