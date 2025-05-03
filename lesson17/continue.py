fc91b692a0fbb69c3db38e364d5d7c57e8a3e282
1nawaalcoding <126.nawaal-mir@foundationworldschool.com>
2025-05-03 17:30:57 +0530

    lesson17

---
+++ lesson17/continue.py
@@ -0,0 +1,7 @@
+v=10
+while v>0:
+    v=v-1
+    if v==5:
+        continue
+    print("the current variable is",v)
+print("byee")
---
+++ lesson17/pass.py
@@ -0,0 +1,11 @@
+for x in range(20):
+    if x%20==0:
+        print("hi")
+    elif x%15==0:
+        pass
+    elif x%5==0:
+        print("why")
+    elif x%3==0:
+        print("turn")
+    else:
+        print(x)
