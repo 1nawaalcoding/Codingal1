257f5fafdc9736d9222153c34366ea77000d0ff6
1nawaalcoding <126.nawaal-mir@foundationworldschool.com>
2025-05-03 17:41:07 +0530

    project 17.py

---
+++ lesson17/continue2.py
@@ -0,0 +1,7 @@
+for i in range(20):
+    if i % 10 == 0:
+        print(i)
+        continue
+    print("the current value is:",i)
+
+    
---
+++ lesson17/project 17.py
@@ -0,0 +1,5 @@
+price=10.0
+def amount(amount_given):
+    return price-amount_given
+a=amount(5.0)
+print("the due amount is", a)
